1ffc1b13911b54fadba69fdf126b7da379b48c30
Vincent Rischmann <vincent@rischmann.fr>
2022-12-13 22:38:24 +0100

    add the main entrypoint, build the axum server for the application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use secrecy::Secret;
+use servare::startup::get_connection_pool;
+use servare::startup::DashboardApplication;
+use std::str::FromStr;
+
+fn main() -> anyhow::Result<()> {
+    // Build the Tokio runtime
+
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .worker_threads(4)
+        .thread_name("servare")
+        .thread_stack_size(3 * 1024 * 1024)
+        .enable_all()
+        .build()
+        .unwrap();
+    let _runtime_guard = runtime.enter();
+
+    // TODO(vincent): replace me
+    let connection_string =
+        Secret::from_str("postgresql://vincent:vincent@localhost/servare_tests")?;
+
+    let dashboard_pool = runtime.block_on(get_connection_pool(connection_string));
+    let dashboard_app = DashboardApplication::build_with_pool(dashboard_pool)?;
+
+    let future = dashboard_app.run_until_stopped();
+
+    println!("running dashboard app");
+
+    // Run the app future until done
+    let _ = runtime.block_on(future);
+
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,12 +1,75 @@
-use std::io;
+use axum::routing::IntoMakeService;
+use hyper::server::conn::AddrIncoming;
+use secrecy::{ExposeSecret, Secret};
+use sqlx::postgres::PgPoolOptions;
+use sqlx::PgPool;
+use std::time::Duration;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("hyper server failed")]
+    Hyper(#[from] hyper::Error),
+    #[error("unable to bind tcp listener")]
+    IO(#[from] std::io::Error),
+    #[error(transparent)]
+    Unexpected(#[from] anyhow::Error),
+}
+
+type Server = axum::Server<AddrIncoming, IntoMakeService<axum::Router>>;
 
 pub struct DashboardApplication {
     pub port: u16,
     pub pool: sqlx::PgPool,
+    server: Server,
 }
 
 impl DashboardApplication {
-    pub async fn build_with_pool(pool: sqlx::PgPool) -> Result<DashboardApplication, io::Error> {
-        Ok(DashboardApplication { port: 2000, pool })
+    pub fn build_with_pool(pool: sqlx::PgPool) -> Result<DashboardApplication, Error> {
+        let listener = std::net::TcpListener::bind(&format!("{}:{}", "127.0.0.1", 4052))
+            .map_err(Into::<Error>::into)?;
+        let port = listener.local_addr().unwrap().port();
+
+        let server: Server = create_server(listener)?;
+
+        Ok(DashboardApplication { port, pool, server })
     }
+
+    pub async fn run_until_stopped(self) -> Result<(), Error> {
+        self.server.await?;
+
+        Ok(())
+    }
+}
+
+async fn fallback_handler() -> (http::StatusCode, String) {
+    (http::StatusCode::NOT_FOUND, "Page Not Found".to_owned())
+}
+
+async fn foobar() -> impl axum::response::IntoResponse {
+    "foobar".to_string()
+}
+
+fn create_server(listener: std::net::TcpListener) -> Result<Server, anyhow::Error> {
+    let state = ();
+
+    let web_app = axum::Router::new()
+        .route("/foobar", axum::routing::get(foobar))
+        .fallback(fallback_handler)
+        .layer(tower_http::trace::TraceLayer::new_for_http())
+        .with_state(state);
+
+    let web_server_builder = axum::Server::from_tcp(listener)?;
+
+    let web_server = web_server_builder.serve(web_app.into_make_service());
+
+    Ok(web_server)
+}
+
+pub async fn get_connection_pool(connection_string: Secret<String>) -> PgPool {
+    PgPoolOptions::new()
+        .max_connections(1024)
+        .acquire_timeout(Duration::from_secs(1))
+        .connect(connection_string.expose_secret())
+        .await
+        .expect("Failed to connect to PostgreSQL")
 }
