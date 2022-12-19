4eea9d44c86da3b9bacad37a97b6b40a6356bfbe
Vincent Rischmann <vincent@rischmann.fr>
2022-12-19 21:43:40 +0100

    tests/dashboard: add tests for / and /login

--- tests/dashboard/routes.rs
+++ tests/dashboard/routes.rs
@@ -1,9 +1,23 @@
 use crate::helpers::spawn_app;
 
 #[tokio::test]
-async fn foobar_should_work() {
+async fn home_should_work() {
     let app = spawn_app().await;
 
-    let response = app.get_foobar_html().await;
-    assert_eq!(&response, "foobar");
+    let response = app.get_home_html().await;
+    assert!(
+        response.contains("Home"),
+        "home page doesn't contain the title 'Home'"
+    );
+}
+
+#[tokio::test]
+async fn login_should_work() {
+    let app = spawn_app().await;
+
+    let response = app.get_login_html().await;
+    assert!(
+        response.contains("Login"),
+        "login page doesn't contain the title 'Login'"
+    );
 }
