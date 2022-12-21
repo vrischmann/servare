e208b00160b7f441f1657bf74f61166128d0ea81
Vincent Rischmann <vincent@rischmann.fr>
2022-12-21 22:07:11 +0100

    routes/login: extract the login form data; add todo

--- src/routes/login.rs
+++ src/routes/login.rs
@@ -1,6 +1,7 @@
 use crate::domain::User;
 use crate::routes::{see_other, Error};
 use askama::Template;
+use axum::extract::Form;
 use axum::response::{Html, IntoResponse};
 
 #[derive(askama::Template)]
@@ -17,7 +18,20 @@ pub async fn form() -> Result<Html<String>, Error> {
     Ok(response)
 }
 
-/// TODO(vincent): implement me
-pub async fn submit() -> impl IntoResponse {
+#[derive(serde::Deserialize)]
+pub struct LoginFormData {
+    pub email: String,
+}
+
+pub async fn submit(Form(login_form_data): Form<LoginFormData>) -> impl IntoResponse {
+    // TODO(vincent): implement this !
+    //
+    // 1) lookup the account with the email
+    //
+    // 2) if the account exists, determine if it has alternative login methods (password, webauthn)
+    // 3) if it has any, redirect to a page with buttons to choose the login method (email,
+    //    password or webauthn)
+    // 4) if not, send a login email
+
     see_other("/")
 }
