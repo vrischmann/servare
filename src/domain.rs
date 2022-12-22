bbf63014f68869397f22838aa94e1cfb111ff85b
Vincent Rischmann <vincent@rischmann.fr>
2022-12-22 11:28:59 +0100

    domain: rename UserID to UserId
    In Rust they don't capitalize acronyms like in Go

--- src/domain.rs
+++ src/domain.rs
@@ -2,14 +2,14 @@ use uuid::Uuid;
 
 #[derive(sqlx::Type)]
 #[sqlx(transparent)]
-pub struct UserID(Uuid);
+pub struct UserId(Uuid);
 
 #[derive(sqlx::Type)]
 #[sqlx(transparent)]
 pub struct UserEmail(String);
 
 pub struct User {
-    pub id: UserID,
+    pub id: UserId,
     pub email: UserEmail,
 }
 
