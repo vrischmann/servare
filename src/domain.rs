7b69f3c4c0a5a6cd08126e7862cbefbe3c6f4eba
Vincent Rischmann <vincent@rischmann.fr>
2022-12-22 11:42:09 +0100

    domain: add serde derives, implement AsRef<str>
    serde derives are necessary, among other things, to:
    * serialize/deserialize to JSON
    * serialize/deserialize to a URL-encoded form
    
    AsRef<str> lets us get the value of the email directly.

--- src/domain.rs
+++ src/domain.rs
@@ -1,12 +1,18 @@
 use uuid::Uuid;
 
-#[derive(sqlx::Type)]
+#[derive(sqlx::Type, serde::Deserialize, serde::Serialize)]
 #[sqlx(transparent)]
 pub struct UserId(Uuid);
 
-#[derive(sqlx::Type)]
+#[derive(sqlx::Type, serde::Deserialize, serde::Serialize)]
 #[sqlx(transparent)]
 pub struct UserEmail(String);
+
+impl AsRef<str> for UserEmail {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
 
 pub struct User {
     pub id: UserId,
