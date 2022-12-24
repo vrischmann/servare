54a648dc8f7434db47917a4def4f5fa7697a7bd4
Vincent Rischmann <vincent@rischmann.fr>
2022-12-24 12:41:15 +0100

    routes/login: remove the 'alternative login' specific page

--- src/routes/login.rs
+++ src/routes/login.rs
@@ -2,13 +2,9 @@ use crate::domain::{User, UserEmail};
 use crate::routes::{see_other, Error};
 use crate::startup::ApplicationState;
 use askama::Template;
-use axum::body::{Empty, Full};
 use axum::extract::{Form, State};
-use axum::http::header;
-use axum::http::StatusCode;
-use axum::response::{Html, IntoResponse, Response};
+use axum::response::{Html, IntoResponse};
 use sqlx::PgPool;
-use tracing::error;
 
 #[derive(askama::Template)]
 #[template(path = "login.html.j2")]
@@ -16,19 +12,13 @@ struct LoginTemplate {
     pub user: Option<User>,
 }
 
+#[tracing::instrument(name = "Login form")]
 pub async fn form() -> Result<Html<String>, Error> {
     let tpl = LoginTemplate { user: None };
 
     let response = Html(tpl.render()?);
 
     Ok(response)
-}
-
-#[derive(askama::Template)]
-#[template(path = "login_alternative_methods.html.j2")]
-struct LoginAlternativeMethodsTemplate {
-    pub user: Option<User>,
-    pub has_password: bool,
 }
 
 #[derive(serde::Deserialize)]
@@ -41,36 +31,31 @@ pub async fn submit(
     State(state): State<ApplicationState>,
     Form(form_data): Form<LoginFormData>,
 ) -> Result<impl IntoResponse, Error> {
-    let pool = &state.pool;
+    let _pool = &state.pool;
 
     tracing::Span::current().record("email", &tracing::field::display(&form_data.email));
 
-    match fetch_user_login_methods(pool, &form_data.email).await {
-        Ok(methods) => {
-            if methods.email && methods.password {
-                let tpl = LoginAlternativeMethodsTemplate {
-                    user: None,
-                    has_password: true,
-                };
+    Ok(see_other("/"))
 
-                // TODO(vincent): error handling for this template render
-                let tpl_rendered = tpl.render().unwrap();
+    // match fetch_user_login_methods(pool, &form_data.email).await {
+    //     Ok(methods) => {
+    //         if methods.email && methods.password {
+    //             // TODO(vincent): need to keep the email in a session
+    //             let response = see_other("/login/alternative").into_response();
 
-                let response = Html(tpl_rendered).into_response();
+    //             Ok(response)
+    //         } else {
+    //             // TODO(vincent): implement sending the login email
+    //             let response = see_other("/").into_response();
 
-                Ok(response)
-            } else {
-                // TODO(vincent): implement sending the login email
-                let response = see_other("/").into_response();
-
-                Ok(response)
-            }
-        }
-        Err(err) => {
-            error!(err = %err, "unable to check if user exists");
-            Err(err.into())
-        }
-    }
+    //             Ok(response)
+    //         }
+    //     }
+    //     Err(err) => {
+    //         error!(err = %err, "unable to check if user exists");
+    //         Err(err.into())
+    //     }
+    // }
 }
 
 #[derive(Debug, PartialEq)]
