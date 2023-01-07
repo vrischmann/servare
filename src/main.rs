464f4871443c180683a4e8bc424ceb37bcabc679
Vincent Rischmann <vincent@rischmann.fr>
2023-01-08 00:27:27 +0100

    main: introduce subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
-use servare::configuration::get_configuration;
+use read_input::InputBuild;
+use secrecy::Secret;
+use servare::authentication::create_user;
+use servare::configuration::{get_configuration, Config};
+use servare::domain::UserEmail;
 use servare::startup::get_connection_pool;
 use servare::startup::Application;
 use servare::telemetry;
 use tracing::{error, info};
 
-fn main() {
-    let config = match get_configuration() {
-        Ok(config) => config,
-        Err(err) => {
-            error!(err = %err, "unable to get the configuration");
-            std::process::exit(1)
-        }
-    };
-
+async fn run_serve(config: Config, _matches: &clap::ArgMatches) -> anyhow::Result<()> {
+    // Setup
     let subscriber = telemetry::get_subscriber(
         telemetry::Configuration {
             jaeger_config: config.jaeger,
@@ -21,6 +18,73 @@ fn main() {
         std::io::stdout,
     );
     telemetry::init_global_default(subscriber);
+
+    let pool = get_connection_pool(&config.database).await?;
+    let app = Application::build(&config.application, &config.session, pool)?;
+
+    info!(
+        url = format!(
+            "{}:{}",
+            config.application.base_url, config.application.port
+        ),
+        "running dashboard app"
+    );
+
+    // Finally start the application
+    app.run_until_stopped().await?;
+
+    Ok(())
+}
+
+async fn run_users(config: Config, matches: &clap::ArgMatches) -> anyhow::Result<()> {
+    match matches.subcommand() {
+        Some(("setup-admin", matches)) => {
+            // Email comes from the cli arguments
+            let email = {
+                let tmp = matches.get_one::<String>("email").unwrap();
+                UserEmail::parse(tmp.to_string())?
+            };
+
+            // Password is read from the terminal
+            let password = {
+                let tmp = read_input::prelude::input::<String>()
+                    .msg("Password: ")
+                    .get();
+
+                Secret::new(tmp)
+            };
+
+            let pool = get_connection_pool(&config.database).await?;
+
+            // Create the admin user
+            let user_id = create_user(&pool, &email, password).await?;
+
+            println!("created user {}. id={}", email, user_id);
+
+            Ok(())
+        }
+        _ => Ok(()),
+    }
+}
+
+async fn run_commands(config: Config, matches: &clap::ArgMatches) -> anyhow::Result<()> {
+    match matches.subcommand() {
+        Some(("users", matches)) => run_users(config, matches).await?,
+        Some(("serve", matches)) => run_serve(config, matches).await?,
+        _ => unreachable!("should never happen because of subcommand_required"),
+    }
+    Ok(())
+}
+
+fn main() {
+    // Always read the configuration
+    let config = match get_configuration() {
+        Ok(config) => config,
+        Err(err) => {
+            error!(err = %err, "unable to get the configuration");
+            std::process::exit(1)
+        }
+    };
 
     // Build the Tokio runtime
     let runtime = tokio::runtime::Builder::new_current_thread()
@@ -32,40 +96,37 @@ fn main() {
         .unwrap();
     let _runtime_guard = runtime.enter();
 
-    // Connect to PostgreSQL and get a connection pool
-    let pool = match runtime.block_on(get_connection_pool(&config.database)) {
-        Ok(pool) => pool,
-        Err(err) => {
-            error!(err = %err, "unable to get a connection pool");
-            std::process::exit(1)
-        }
-    };
+    // Parse the command line arguments to know what to do
+    let root_command = clap::Command::new("servare")
+        .version(clap::crate_version!())
+        .about("Servare")
+        .subcommand_required(true)
+        .subcommand(
+            clap::Command::new("users")
+                .about("Manage users of Sercare")
+                .subcommand_required(true)
+                .subcommand(
+                    clap::Command::new("setup-admin")
+                        .about("Setup the admin user")
+                        .arg(
+                            clap::Arg::new("email")
+                                .help("The admin user email")
+                                .action(clap::ArgAction::Set)
+                                .value_name("EMAIL")
+                                .required(true),
+                        ),
+                ),
+        )
+        .subcommand(clap::Command::new("serve").about("Serve the application"));
 
-    // Build the web application
-    let app = match Application::build(&config.application, &config.session, pool) {
-        Ok(app) => app,
-        Err(err) => {
-            error!(err = %err, "failed to build application");
-            std::process::exit(1)
-        }
-    };
+    let matches = root_command.get_matches();
+    let future = run_commands(config, &matches);
 
-    // Finally start the application asynchronously
-    let future = app.run_until_stopped();
+    // Run the future until done
+    let result = runtime.block_on(future);
 
-    info!(
-        url = format!(
-            "{}:{}",
-            config.application.base_url, config.application.port
-        ),
-        "running dashboard app"
-    );
-
-    // Run the app future until done
-    match runtime.block_on(future) {
-        Ok(()) => {}
-        Err(err) => {
-            error!(err = %err, "application failed");
-        }
+    if let Err(err) = result {
+        println!("{}", err);
+        std::process::exit(1);
     }
 }
